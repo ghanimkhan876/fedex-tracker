3d63624d11c54d9a76b8b28b0cf4a363bd374446
ghanim khan <you@example.com>
2018-10-08 01:13:14 +0530

    improverd the performance of the code by importing the files to the code

--- build.py
+++ build.py
@@ -1,4 +1,15 @@
 from distutils.core import setup
+from selenium import webdriver
+from bs4 import BeautifulSoup
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+import time
+
 import py2exe
 
 setup(console=['trackingtimes.py'])
+html=browser.page_source
+soup=BeautifulSoup(html,"lxml")
+print soup.prettify()
